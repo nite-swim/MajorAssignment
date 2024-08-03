196c3237055918e0ec63b87071f8222e68f0746d
laoma023012 <974958656@qq.com>
2024-08-03 19:48:16 +0800

    add data

---
+++ student/maxiaochen/main.java
@@ -0,0 +1,8 @@
+package maxiaochen;
+import static maxiaochen.userLoadSystem.*;
+public class main {
+    public static void main(String[] args) {
+        userLoadSystem sysData = new userLoadSystem();
+        sysData.authenticationPasswd();
+    }
+}
